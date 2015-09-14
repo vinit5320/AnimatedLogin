fa04873ff27cd7133d7bb12fe2fe16617cf78a20
Vinit Jasoliya <ViNiT@Vinits-MBP.Home>
2015-09-14 22:02:11 +0530

    Initial Commit

---
+++ AnimatedLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimatedLogin
+//
+//  Created by Vinit Jasoliya on 9/14/15.
+//  Copyright (c) 2015 ViNiT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
